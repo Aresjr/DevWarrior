73a59dcd571b0549dc3f19069ff349a71a844084
AresJr <aresnemeia@gmail.com>
2024-06-06 10:13:29 -0300

    Fixed initializr test

--- test/conftest.py
+++ test/conftest.py
@@ -21,14 +21,3 @@ def client(app):
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
-
-def test_initialize(app):
-    with app.app_context():
-        db.create_all()
-        if db.session.query(SkillCategory).count() == 0:
-            initialize()
-
-        assert db.session.query(Skill).count() > 0
-        assert db.session.query(SkillCategory).count() > 0
-        assert db.session.query(Class).count() > 0
-        assert db.session.query(Title).count() > 0
--- test/test_initializr.py
+++ test/test_initializr.py
@@ -1,14 +1,15 @@
-from app import wapp
 from database import db
 from initializr import initialize
 from models.skills import SkillCategory, Skill
 from models.user import Class, Title
 
 
-def test_initialize_db():
-    with wapp.app_context():
+def test_initialize(app):
+    with app.app_context():
         db.create_all()
-        initialize()
+        if db.session.query(SkillCategory).count() == 0:
+            initialize()
+
         assert db.session.query(Skill).count() > 0
         assert db.session.query(SkillCategory).count() > 0
         assert db.session.query(Class).count() > 0
